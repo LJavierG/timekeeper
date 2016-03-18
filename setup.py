fd206813eef9f0e500a10932e41e5c5fd0777019
LJavierG <luis.j.glez.devel@gmail.com>
2016-03-18 20:48:21 +0000

    setup.py filled

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+	name="timekeeper",
+	version=0.1,
+	url="github.com/ljavierg/timekeeper/",
+	license="GNU General Public License v3.0",
+	author="Luis Javier Gonzalez",
+	description="Keep track of the time used in different tasks and projects, based on worklog.",
+	scripts=["timekeeper.py"],
+	)
+
